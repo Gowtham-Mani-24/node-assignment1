0d7fcc9ee3e868085f4337cb513d07042d5d9424
GOWTHAM-MANI-24 <workspaceofgowtham@gmail.com>
2024-09-07 20:56:18 +0530

    completed assignment1

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req,res) =>{
-    const {url, method} = req;
+const app = express();
+const fs = require('fs');
+const path = require('path');
 
-    if (url =='/'){
-        if(method == 'GET'){
-            return res.end('Get World');
-        }
-        else if(method == 'POST'){
-            return res.end('Post World');
+app.get('/create', (req, res) => {
+    const d = new Date();
+    const dateTime = `Date: ${d.toLocaleDateString()} and Time: ${d.toLocaleTimeString()}`;
+
+    fs.writeFile('./files/date-time.txt', dateTime, (err) => {
+        if (err) {
+            res.send(err);
         }
         else {
-            return res.end('Endpoinr not found');
+            res.send('File created successfully');
         }
-    }
+    })
+});
+
+app.get('/files', (req, res) => {
+    const directoryPath = path.join(__dirname, 'files');
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.send(files);
+        }
+    })
 })
 
-server.listen(3002,'localhost',() => {
+app.listen(3002, 'localhost', () => {
     console.log('Server is running on http://localhost:3002');
-})
+});
